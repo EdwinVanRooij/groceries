48f20b1e105462251360c9c4d351f933d5f1514d
Edwin <edwinvrooij@gmail.com>
2016-11-22 20:48:10 +0100

    finished friend page profile, fixes #11

--- app/src/main/java/me/evrooij/groceries/ProfileActivity.java
+++ app/src/main/java/me/evrooij/groceries/ProfileActivity.java
@@ -1,23 +1,71 @@
 package me.evrooij.groceries;
 
+import android.content.res.ColorStateList;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Build;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.view.ViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.RecyclerView;
-import android.widget.ListView;
-import butterknife.BindView;
+import android.support.v7.graphics.Palette;
+import android.support.v7.widget.Toolbar;
+import android.transition.Slide;
+import android.view.MotionEvent;
+import android.widget.ImageView;
+import android.widget.TextView;
 import butterknife.ButterKnife;
-import me.evrooij.groceries.domain.GroceryList;
-import me.evrooij.groceries.domain.adapters.GroceryListAdapter;
-import me.evrooij.groceries.domain.adapters.UserAdapter;
-
-import java.util.ArrayList;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Callback;
 
 public class ProfileActivity extends AppCompatActivity {
 
+    private static final String EXTRA_IMAGE = "com.antonioleiva.materializeyourapp.extraImage";
+    private CollapsingToolbarLayout collapsingToolbarLayout;
+
+    @SuppressWarnings("ConstantConditions")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        initActivityTransitions();
         setContentView(R.layout.activity_profile);
         ButterKnife.bind(this);
+
+        ViewCompat.setTransitionName(findViewById(R.id.app_bar_layout), EXTRA_IMAGE);
+        supportPostponeEnterTransition();
+
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        collapsingToolbarLayout.setTitle(getResources().getString(R.string.placeholder_name));
+        collapsingToolbarLayout.setExpandedTitleColor(getResources().getColor(android.R.color.transparent));
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent motionEvent) {
+        try {
+            return super.dispatchTouchEvent(motionEvent);
+        } catch (NullPointerException e) {
+            return false;
+        }
+    }
+
+    private void initActivityTransitions() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Slide transition = new Slide();
+            transition.excludeTarget(android.R.id.statusBarBackground, true);
+            getWindow().setEnterTransition(transition);
+            getWindow().setReturnTransition(transition);
+        }
+    }
+
+    private void updateBackground(FloatingActionButton fab, Palette palette) {
+        int lightVibrantColor = palette.getLightVibrantColor(getResources().getColor(android.R.color.white));
+        int vibrantColor = palette.getVibrantColor(getResources().getColor(R.color.color));
+
+        fab.setRippleColor(lightVibrantColor);
+        fab.setBackgroundTintList(ColorStateList.valueOf(vibrantColor));
     }
 }
