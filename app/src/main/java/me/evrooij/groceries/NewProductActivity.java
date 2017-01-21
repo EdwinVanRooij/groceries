ca742dbd47c11237ecc8ebff817967539a91c79c
Edwin <edwinvrooij@outlook.com>
2017-01-21 20:19:58 +0100

    fixed #60 and more or less fixed #55

--- app/src/main/java/me/evrooij/groceries/NewProductActivity.java
+++ app/src/main/java/me/evrooij/groceries/NewProductActivity.java
@@ -4,21 +4,33 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Button;
-import android.widget.EditText;
+import android.util.Log;
+import android.view.View;
+import android.widget.*;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import butterknife.OnTextChanged;
+import me.evrooij.groceries.adapters.MyProductAdapter;
+import me.evrooij.groceries.adapters.ProductAdapter;
 import me.evrooij.groceries.data.Account;
 import me.evrooij.groceries.data.Product;
+import me.evrooij.groceries.data.ProductManager;
 import org.parceler.Parcels;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.R.attr.data;
 import static me.evrooij.groceries.Config.*;
+import static me.evrooij.groceries.R.id.etEmailContainer;
+import static me.evrooij.groceries.R.id.textView;
+import static me.evrooij.groceries.R.id.tvTipDescription;
 
 public class NewProductActivity extends AppCompatActivity {
     //region Fields
     @BindView(R.id.etName)
-    EditText etName;
+    AutoCompleteTextView etName;
     @BindView(R.id.etAmount)
     EditText etAmount;
     @BindView(R.id.etRemark)
@@ -28,6 +40,12 @@ public class NewProductActivity extends AppCompatActivity {
     Button btnAdd;
 
     private Account thisAccount;
+
+    private ProductManager productManager;
+    private ArrayList<Product> data;
+    private MyProductAdapter adapter;
+
+    private Product selectedMyProduct;
 
     private boolean isUpdate;
     private Product thisProduct;
@@ -49,6 +67,33 @@ public class NewProductActivity extends AppCompatActivity {
 
             btnAdd.setText(R.string.button_update_product);
         }
+        if (!isUpdate) {
+            productManager = new ProductManager(this);
+            new Thread(() -> {
+                ArrayList<Product> result = (ArrayList<Product>) productManager.getMyProducts(thisAccount.getId());
+
+                ProductAdapter adapter = new ProductAdapter(this, result);
+
+                runOnUiThread(() -> {
+                    etName.setAdapter(adapter);
+                    etName.setOnItemClickListener((parent, view, position, id) -> {
+                        selectedMyProduct = (Product) parent.getItemAtPosition(position);
+                        etName.setText(selectedMyProduct.getName());
+                        etAmount.setText(String.valueOf(selectedMyProduct.getAmount()));
+                        etRemark.setText(selectedMyProduct.getComment());
+                    });
+                });
+            }).start();
+
+        }
+    }
+
+    private static final String TAG = "NewProductActivity";
+
+    @OnTextChanged(R.id.etName)
+    protected void onTextChanged(CharSequence text) {
+        String featureName = text.toString();
+        Log.i(TAG, String.format("feature name: %s", featureName));
     }
 
     @OnClick(R.id.btnAdd)
@@ -73,7 +118,14 @@ public class NewProductActivity extends AppCompatActivity {
             setResult(Activity.RESULT_OK, returnIntent);
             finish();
         } else {
-            product = new Product(name, amount, comment, thisAccount);
+            if (selectedMyProduct == null) {
+                product = new Product(name, amount, comment, thisAccount);
+            } else {
+                selectedMyProduct.setName(name);
+                selectedMyProduct.setAmount(amount);
+                selectedMyProduct.setComment(comment);
+                product = selectedMyProduct;
+            }
             // Product doesn't exist yet, make new product
             Intent returnIntent = new Intent();
             returnIntent.putExtra(KEY_NEW_PRODUCT, Parcels.wrap(product));
