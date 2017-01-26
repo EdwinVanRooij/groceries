3f7fde4aa783b633bc79a4b8726d4dd2c8afe417
Edwin <edwinvrooij@outlook.com>
2017-01-26 15:07:01 +0100

    completed feedback update to kt

--- app/src/main/java/me/evrooij/groceries/data/Feedback.kt
+++ app/src/main/java/me/evrooij/groceries/data/Feedback.kt
@@ -6,9 +6,12 @@ import com.google.gson.annotations.SerializedName
  * Created by eddy on 27-11-16.
  */
 
-class Feedback(var message: String, var type: Type, var sender: Account) {
+class Feedback(
+        var message: String,
+        @SerializedName("type") var type: Type,
+        var sender: Account) {
 
-    val sender: Account
+    var id: Int = 0
 
     fun Feedback() {
 
