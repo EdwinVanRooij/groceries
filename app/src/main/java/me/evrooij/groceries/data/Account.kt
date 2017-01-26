ec3707d95b17549289bd03a3b03c72be466890a6
Edwin <edwinvrooij@outlook.com>
2017-01-26 14:57:53 +0100

    updated account to kotlin

---
+++ app/src/main/java/me/evrooij/groceries/data/Account.kt
@@ -0,0 +1,17 @@
+package me.evrooij.groceries.data;
+
+import org.parceler.Parcel;
+
+/**
+ * Author: eddy
+ * Date: 27-11-16.
+ */
+
+@Parcel
+class Account(var id: Int, var username: String, var email: String) {
+
+    fun Account() {
+    }
+
+    override fun toString(): String = String.format("User %s (%s) - Mail %s", username, id, email)
+}
